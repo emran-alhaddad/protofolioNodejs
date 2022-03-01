179984077f719add07ef1a6f5520c47548b4223f
EnasHameed <alqaadienas@gmail.com>
2022-03-01 16:24:38 +0300

    change const port to host with heroku & add Procfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.listen(3000);
+
+const port = process.env.Port || 3000
 app.use(express.static('./assets'))
 app.set('view engine', 'ejs');
 
@@ -10,3 +11,5 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.send('notfound');
 });
+
+app.listen(port, () => console.log('listening on port ${port}'));
